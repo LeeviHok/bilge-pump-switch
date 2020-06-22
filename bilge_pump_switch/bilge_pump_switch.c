d4729660d40ed85a81d6d19d60b9c9102a6cf82f
Leevi Hokkanen <leevi.sahkoposti@gmail.com>
2020-06-22 23:58:23 +0300

    Added AtmelStudio project for ATmega328P with empty source file.

---
+++ bilge_pump_switch/bilge_pump_switch.c
@@ -0,0 +1,17 @@
+/*
+ * bilge_pump_switch.c
+ *
+ * Created: 10/06/2020 11.29.35
+ * Author : Leevi Hokkanen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
